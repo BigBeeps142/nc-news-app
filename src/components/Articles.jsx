595529907d2fd58548c5b894ec446c4bc2438bed
Ben Lomas <benlomas7@outlook.com>
2022-03-07 14:38:58 +0000

    Added loading message

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -4,14 +4,18 @@ import Articlecard from "./ArticleCard";
 
 const Articles = ({ topic }) => {
   const [articles, setArticles] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
+    setIsLoading(true);
     getArticles().then((data) => {
+      setIsLoading(false);
       setArticles(data.articles);
     });
   }, [topic]);
 
-  return (
+  return isLoading ? (
+    <h2>Loading...</h2>
+  ) : (
     <ul className="ArticleList">
       {articles.map((article) => {
         return <Articlecard key={article.title} article={article} />;
