2a5e6a532470098a37a0cea3bc01b06f7690f47a
Ben Lomas <benlomas7@outlook.com>
2022-03-07 11:37:17 +0000

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App"></div>;
+      <div className="App">
+        <Header />
+      </div>
     </BrowserRouter>
   );
 }
