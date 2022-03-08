dc096f497e9e61b2df542ce1fd38c3e1b0ade593
Ben Lomas <benlomas7@outlook.com>
2022-03-08 11:51:00 +0000

    Added loading message

--- src/components/Articles/Article.jsx
+++ src/components/Articles/Article.jsx
@@ -4,14 +4,19 @@ import { getArticle } from "../../api";
 
 const Article = () => {
   const [article, setArticle] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const { article_id } = useParams();
 
   useEffect(() => {
+    setIsLoading(true);
     getArticle(article_id).then(({ article }) => {
+      setIsLoading(false);
       setArticle(article);
     });
   }, [article_id]);
-  return (
+  return isLoading ? (
+    <h2>Loading...</h2>
+  ) : (
     <section className="Article">
       <h2 className="Article-Title">{article.title}</h2>
       <h2 className="Article-Topic">{article.topic}</h2>
