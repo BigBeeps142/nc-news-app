2ba4770b23be5a6fd193bd01fe37377b50497836
Ben Lomas <benlomas7@outlook.com>
2022-03-10 12:02:12 +0000

    Added user context

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Article from "./components/Articles/Article";
 import Articlespage from "./components/Articles/ArticlesPage";
 import Header from "./components/Header";
 import Pagenav from "./components/PageNav";
+import { UserContext } from "./contexts/UserContext";
 
 function App() {
+  const [user, setUser] = useState({});
   return (
     <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Pagenav />
-        <Routes>
-          <Route path="/articles" element={<Articlespage />} />
-          <Route path="/articles/:topic" element={<Articlespage />} />
-          <Route path="/articles/article/:article_id" element={<Article />} />
-        </Routes>
-      </div>
+      <UserContext.Provider value={{ user, setUser }}>
+        <div className="App">
+          <Header />
+          <Pagenav />
+          <Routes>
+            <Route path="/articles" element={<Articlespage />} />
+            <Route path="/articles/:topic" element={<Articlespage />} />
+            <Route path="/articles/article/:article_id" element={<Article />} />
+          </Routes>
+        </div>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
